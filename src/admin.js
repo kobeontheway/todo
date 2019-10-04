d3fe887f95a0539a25d0b4d11f29edb56ecccd2d
tongxin.yuan <tongxin.yuan@bianlifeng.com>
2019-10-05 01:27:49 +0800

    a

--- src/admin.js
+++ src/admin.js
@@ -2,24 +2,22 @@ import React from 'react';
 import {Row, Col} from 'antd';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import NavLeft from './components/NavLeft';
+import './style/common.less';
+import menu from './config/menuConfig';
 export default class Admin extends React.Component{
     render(){
         return (
-            <Row>   
-                <Col span="3">
-                    Left
+            <Row className="container">   
+                <Col span="3" className="nav-left">
+                    <NavLeft/>
                 </Col>
-                <Col span="21">
-                    <Header>
-                        Header
-                    </Header>
-                    Right
-                    <Row>
+                <Col span="21" className="main">
+                    <Header/>
+                    <Row className="content">
                         content
                     </Row>
-                    <Footer>
-                        Footer
-                    </Footer>
+                    <Footer/>
                 </Col>
             </Row>
         );
